0a3cd6264db7ae5c89970405ddfbff2d21fee627
MEGAN ARNELL <arnell.megan@yahoo.com>
2022-05-29 14:10:37 -0600

    Added basic nav bar using material ui

--- src/index.js
+++ src/index.js
@@ -2,194 +2,33 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link
+    BrowserRouter as Router,
+    Routes,
+    Route
 } from 'react-router-dom';
 import Home from './components/home'
 import About from './components/about'
 import Contact from './components/contact'
 import TicTacToeGame from './components/tic-tac-toe'
+import Navbar from './components/navbar'
 
 class App extends Component {
     render() {
         return (
-        <Router>
-            <div className="App">
-	<ul>
-	<li>
-		<Link to="/">Home</Link>
-	</li>
-	<li>
-		<Link to="/about">About Us</Link>
-	</li>
-	<li>
-		<Link to="/contact">Contact Us</Link>
-	</li>
-    <li>
-		<Link to="/tic-tac-toe">Tic-Tac-Toe</Link>
-	</li>
-	</ul>
-    </div>
-    <Routes>
-        <Route exact path='/' element={< Home />}></Route>
-        <Route exact path='/about' element={< About />}></Route>
-        <Route exact path='/contact' element={< Contact />}></Route>
-        <Route exact path='/tic-tac-toe' element={< TicTacToeGame />}></Route>
-    </Routes>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route exact path='/' element={< Home />}></Route>
+                    <Route exact path='/about' element={< About />}></Route>
+                    <Route exact path='/contact' element={< Contact />}></Route>
+                    <Route exact path='/tic-tac-toe' element={< TicTacToeGame />}></Route>
+                </Routes>
 
-    </Router>
-    );
+            </Router>
+        );
     }
 }
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-
-
-
-// const Square = ({value, onClick}) => {
-//     return (
-//     <button 
-//         className="square" 
-//         onClick={onClick}
-//     >
-//         {value}
-//     </button>
-//     );
-// }
-  
-//   class Board extends React.Component {
-    
-
-//     renderSquare(i) {
-//       return (
-//         <Square 
-//             value={this.props.squares[i]}
-//             onClick={() => this.props.onClick(i)}
-//         />
-//       );
-//     }
-  
-//     render() {
-//       return (
-//         <div className="board">
-//           <div className="board-row">
-//             {this.renderSquare(0)}
-//             {this.renderSquare(1)}
-//             {this.renderSquare(2)}
-//           </div>
-//           <div className="board-row">
-//             {this.renderSquare(3)}
-//             {this.renderSquare(4)}
-//             {this.renderSquare(5)}
-//           </div>
-//           <div className="board-row">
-//             {this.renderSquare(6)}
-//             {this.renderSquare(7)}
-//             {this.renderSquare(8)}
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
-  
-//   class Game extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//           history: [{
-//             squares: Array(9).fill(null),
-//           }],
-//           stepNumber: 0,
-//           xIsNext: true,
-//         };
-//       }
-
-//       handleClick(i) {
-//         const history = this.state.history.slice(0, this.state.stepNumber + 1)
-//         const current = history[history.length-1]
-//         const squares = current.squares.slice()
-//         if (calculateWinner(squares) || squares[i]) {
-//             return;
-//           }
-//         squares[i] = this.state.xIsNext ? 'X' : 'O';
-//         this.setState({
-//             history: history.concat([{
-//                 squares: squares,
-//             }]),
-//             stepNumber: history.length,
-//             xIsNext: !this.state.xIsNext
-//         });
-//     }
-
-//     jumpTo(step) {
-//         this.setState({
-//             stepNumber: step,
-//             xIsNext: (step % 2) === 0
-//         })
-//     }
-
-//     render() {
-//         const history = this.state.history;
-//         const current = history[this.state.stepNumber]
-//         const winner = calculateWinner(current.squares);
-
-//         const moves = history.map((step,move) => {
-//             const desc = move ?
-//                 'Go to move #' + move :
-//                 'Go to game start';
-//             return (
-//                 <li key={move}>
-//                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
-//                 </li>
-//             )
-//         })
-
-//         let status;
-//         if (winner) {
-//             status = 'Winner: ' + winner;
-//         } else {
-//             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-//         }
-
-//       return (
-//         <div className="game">
-//           <div className="game-board">
-//             <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
-//           </div>
-//           <div className="game-info">
-//             <div>{status}</div>
-//             <ol>{moves}</ol>
-//           </div>
-//         </div>
-//       );
-//     }
-//   }
-
-//   function calculateWinner(squares) {
-//     const lines = [
-//       [0, 1, 2],
-//       [3, 4, 5],
-//       [6, 7, 8],
-//       [0, 3, 6],
-//       [1, 4, 7],
-//       [2, 5, 8],
-//       [0, 4, 8],
-//       [2, 4, 6],
-//     ];
-//     for (let i = 0; i < lines.length; i++) {
-//       const [a, b, c] = lines[i];
-//       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//         return squares[a];
-//       }
-//     }
-//     return null;
-//   }
-  
-  // ========================================
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<App />);
-  
-  

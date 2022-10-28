11074016e5cfb6d6d0a4f4b0a326d32fbed32051
MEGAN ARNELL <arnell.megan@yahoo.com>
2022-10-28 13:20:33 -0600

    finish styling cells/buttons

--- src/components/tic-tac-toe-grid/Cell.js
+++ src/components/tic-tac-toe-grid/Cell.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import { Button, makeStyles } from '@material-ui/core'
 
 export default function Cell({ cell, onClick }) {
   const classes = useStyles()
 
   return (
     <div className={classes.cellStyle}>
-      <button type="button" onClick={onClick}>
+      <Button type="button" className={classes.buttonStyle} onClick={onClick}>
         {cell}
-      </button>
+      </Button>
     </div>
   )
 }
 
 const useStyles = makeStyles(() => ({
+  buttonStyle: {
+    width: '100%',
+    height: '100%',
+  },
   cellStyle: {
     backgroundColor: '#fff',
     height: 150,

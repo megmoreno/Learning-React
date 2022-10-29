90c8b4cef84059c4562b348409b695016a96e46f
MEGAN ARNELL <arnell.megan@yahoo.com>
2022-10-28 20:16:54 -0600

    checks for win or draw

--- src/components/tic-tac-toe-grid/TicTacToeGrid.js
+++ src/components/tic-tac-toe-grid/TicTacToeGrid.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Button, makeStyles } from '@material-ui/core'
 import Cell from './Cell'
 
 export default function TicTacToeGrid() {
@@ -10,18 +10,73 @@ export default function TicTacToeGrid() {
   ]
   const [grid, setGrid] = useState(initialGrid)
   const [turn, setTurn] = useState('X')
+  const [moveCount, setMoveCount] = useState(0)
+  const [hasWonGame, setHasWonGame] = useState(false)
+
+  if (hasWonGame) {
+    alert(turn + ' wins!')
+    setMoveCount(0)
+  } else if (moveCount === 9) {
+    alert("It's a draw")
+    setMoveCount(0)
+  }
 
   const onClick = (location) => {
+    if (grid[location.rowIndex][location.colIndex] !== null) return
     const newGrid = [...grid]
     newGrid[location.rowIndex][location.colIndex] = turn
     setGrid(newGrid)
+    setMoveCount((prev) => prev + 1)
+    setHasWonGame(checkForCurrentTurnWin(location))
     setTurn(turn === 'X' ? 'O' : 'X')
   }
 
   const classes = useStyles()
 
+  const checkForCurrentTurnWin = (location) => {
+    //check row
+    let hasWonRow = grid[location.rowIndex].every((value) => {
+      if (value !== turn) {
+        return false
+      }
+      return true
+    })
+    if (hasWonRow) return true
+
+    //check column
+    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+      if (grid[rowIndex][location.colIndex] !== turn) {
+        break
+      } else if (rowIndex == 2) {
+        return true
+      }
+    }
+
+    //check diagonals
+    if (grid[0][0] == turn && grid[1][1] == turn && grid[2][2] == turn)
+      return true
+    if (grid[0][2] == turn && grid[1][1] == turn && grid[2][0] == turn)
+      return true
+
+    return false
+  }
+
   return (
     <div className={classes.container}>
+      <div className={classes.header}>
+        <h1>Tic-Tac-Toe</h1>
+        <h3>Next up: {turn}</h3>
+        <Button
+          type="button"
+          variant="outlined"
+          onClick={() => {
+            setGrid(initialGrid)
+            setMoveCount(0)
+          }}
+        >
+          Reset Game
+        </Button>
+      </div>
       <div className={classes.gridWrapper}>
         <div className={classes.gameGrid}>
           {grid.map((row, rowIndex) =>
@@ -43,6 +98,9 @@ const useStyles = makeStyles(() => ({
   container: {
     textAlign: 'center',
     marginTop: '80px',
+  },
+  header: {
+    marginBottom: '15px',
   },
   gridWrapper: {
     display: 'inline-block',
